9f645155a0867277e4f0004a0a9e72ab66e6785c
phanudet2016 <5706021622141@fitm.kmutnb.ac.th>
2016-11-27 22:53:05 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser')
 var request = require('request')
 var app = express()
 var test = require('./Data.json')
-console.log(test.employees[2].firstName);
+console.log(test.firstName[2]);
 
 app.use(bodyParser.json())
 app.set('port', (process.env.PORT || 4000))
