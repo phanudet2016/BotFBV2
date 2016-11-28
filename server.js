e2bd1bad4ab627318eaf3f32bada2639d2ab577d
phanudet2016 <5706021622141@fitm.kmutnb.ac.th>
2016-11-28 21:19:25 +0700

    Update server.js

--- server.js
+++ server.js
@@ -186,7 +186,29 @@ function receivedPostback(event) {
     }, 1500)
   } 
   //////////////////////////////////////////////////////////////////
-  
+ /*else if(payload == 'robinson'||payload == 'baannernnam'||payload == 'ChomChol'||payload == 'Add'||payload == 'PalmSweetHome'||payload == 'NamHiang'||payload == 'CafeKantary'){*/
+    setTimeout(function() {
+        for(var i = 0; i < data.bigdata.length; i++) {
+            var obj = data.bigdata[i];
+            if(payload==obj.restaurant){
+              sendTextMessage(senderID, obj.text);
+              setTimeout(function() {
+                sendTextMessage(senderID, "คุณชอบรับประทานอาหารประเภทไหนครับ");
+              }, 1000)
+            } 
+        } 
+    }, 500)
+   setTimeout(function() {
+     if(payload == 'robinson'){menuFoodRobinson(senderID);}
+     else if(payload == 'baannernnam'){menuFoodBaannernnam(senderID);}
+     else if(payload == 'ChomChol'){menuFoodChomChol(senderID);}
+     else if(payload == 'Add'){menuFoodAdd(senderID);}
+     else if(payload == 'PalmSweetHome'){menuFoodPalmSweetHome(senderID);}
+     else if(payload == 'NamHiang'){menuFoodNamHiang(senderID);}
+     else if(payload == 'CafeKantary'){menuFoodCafeKantary(senderID);}
+     else{var result = "";}
+    }, 1500)
+ // }
   ///////////////////// ต้องการทานสิ่งนี้ Cafe//////////////////////////////////////
   else if(payload == 'eatCafeFirst'||payload == 'eatCafeSecond'||payload == 'eatCafeThird'||payload == 'eatCafeFourth'||payload == 'eatCafeFifth'||payload == 'eatCafeSixth'){
     setTimeout(function() {
